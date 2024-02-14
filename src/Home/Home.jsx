9e29d52facee63145ad06fe0a61e13b515844c8d
mdshahadad <social.shahadad@gmail.com>
2024-02-14 13:00:54 +0600

    Navbar added at the home page

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,8 +3,8 @@ import Navbar from "./Navbar/Navbar";
 const Home = () => {
     return (
         <div className="bg-[#010101]">
-            {/* <Navbar></Navbar> */}
-            <div className="flex justify-center">
+            <Navbar></Navbar>
+            <div className="flex justify-center mt-10">
                 <img className="" src="https://i.postimg.cc/V6HSzyZp/IMG-2036.jpg" alt="" />
             </div>
         </div>
--- src/Home/Navbar/Navbar.jsx
+++ src/Home/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import { NavLink } from "react-router-dom";
+import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <div className="text-white text-center">
-            <NavLink>Home</NavLink>
-            <NavLink>About</NavLink>
-            <NavLink>Projects</NavLink>
-            <NavLink>Service</NavLink>
-            <NavLink>Contact</NavLink>
+        <div className="text-white text-center pt-4">
+            <NavLink className="nav-list">Home</NavLink>
+            <NavLink className="nav-list">About</NavLink>
+            <NavLink className="nav-list">Projects</NavLink>
+            <NavLink className="nav-list">Service</NavLink>
+            <NavLink className="nav-list">Contact</NavLink>
         </div>
     );
 };
