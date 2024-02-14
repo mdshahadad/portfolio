ae9490db958642322b6f703a57315df21106283e
mdshahadad <social.shahadad@gmail.com>
2024-02-14 20:01:53 +0600

    Add hover effect at the social icons

--- src/Home/SocialIcons/SocialIcons.jsx
+++ src/Home/SocialIcons/SocialIcons.jsx
@@ -8,14 +8,17 @@ const SocialIcons = () => {
             data-aos-duration="2000"
             data-aos-delay="200"
             className="fixed bottom-0 text-[#bfbfbf] -right-5">
+
             <Link to={`https://www.linkedin.com/in/shahadad/`}>
-                <IoLogoLinkedin className="text-4xl"></IoLogoLinkedin>
+                <IoLogoLinkedin className="text-4xl hover:text-white"></IoLogoLinkedin>
             </Link>
+
             <Link to={`https://github.com/mdshahadad`}>
-                <IoLogoGithub className="text-4xl mt-4"></IoLogoGithub>
+                <IoLogoGithub className="text-4xl mt-4 hover:text-white"></IoLogoGithub>
             </Link>
-            <h4 className="rotate-90 mt-14 text-xl -translate-x-[37px] ">Let's connect</h4>
-            <div className="w-[2px] rounded-full mt-14 block h-28 bg-[#bfbfbf] translate-x-[18px]"></div>
+            
+            <h4 className="rotate-90 mt-14 text-xl -translate-x-[37px] hover:text-white">Let's connect</h4>
+            <div className="w-[2px] rounded-full mt-14 block h-28 bg-[#bfbfbf] translate-x-[18px] hover:text-white"></div>
         </div>
     );
 };
