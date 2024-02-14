f75a15811d66d3f3c619b191d6dffeb0df8826dd
mdshahadad <social.shahadad@gmail.com>
2024-02-14 12:57:42 +0600

    Banner photo added

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,7 +1,12 @@
+import Navbar from "./Navbar/Navbar";
+
 const Home = () => {
     return (
-        <div>
-            <h4>This is hommmmmmmmmmmmmmmme</h4>
+        <div className="bg-[#010101]">
+            {/* <Navbar></Navbar> */}
+            <div className="flex justify-center">
+                <img className="" src="https://i.postimg.cc/V6HSzyZp/IMG-2036.jpg" alt="" />
+            </div>
         </div>
     );
 };
--- src/Home/Navbar/Navbar.jsx
+++ src/Home/Navbar/Navbar.jsx
@@ -1,7 +1,13 @@
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className="text-white text-center">
+            <NavLink>Home</NavLink>
+            <NavLink>About</NavLink>
+            <NavLink>Projects</NavLink>
+            <NavLink>Service</NavLink>
+            <NavLink>Contact</NavLink>
         </div>
     );
 };
