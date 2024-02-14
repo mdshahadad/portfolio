c708951704ef560847c8cb52fbc469bd1d566809
mdshahadad <social.shahadad@gmail.com>
2024-02-15 01:56:18 +0600

    Logo, Menu and Hello button style set for responsive design

--- src/Home/Navbar/Navbar.jsx
+++ src/Home/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom";
 import { FaPaperPlane } from "react-icons/fa";
-import { HiMenuAlt3 } from "react-icons/hi";
+import { HiMenuAlt2 } from "react-icons/hi";
 import { RxCross2 } from "react-icons/rx";
 import '../Home.css'
 import { useState } from "react";
@@ -9,29 +9,33 @@ const Navbar = () => {
     const [showMenu, setShowMenu] = useState(false);
     console.log(showMenu)
     return (
-        <div className="flex justify-between max-w-7xl mx-auto items-center py-2 relative overflow-x-visible">
-            <h2 className="text-white opacity-75 font-bold text-4xl">shahadad.</h2>
-            <div className="text-white text-5xl md:hidden">
-                {
-                    !showMenu ?
-                        <HiMenuAlt3 onClick={() => setShowMenu(true)}></HiMenuAlt3>
-                        :
-                        <RxCross2 onClick={() => setShowMenu(false)}></RxCross2>
-                }
-            </div>
+        <div className="block md:flex md:justify-between max-w-7xl md:mx-auto items-center py-2 relative overflow-x-visible">
+            <h2 className="text-white opacity-75 font-bold text-4xl text-center mb-4 md:mb-0">shahadad.</h2>
+
             <div className={`text-white opacity-75 text-center 
             ${showMenu == true ?
-                    "nav-res absolute right-0 top-[50px] rounded-l-lg"
+                    "nav-res absolute -left-2 top-[105px] rounded-r-lg"
                     :
-                    "absolute nav-res-hidden right-0 -top-[130px] rounded-l-lg "}
-            md:block`}>
+                    "absolute nav-res-hidden -left-[130px] top-[105px] rounded-r-lg "}
+            md:block md:top-0`}>
                 <li><NavLink className="nav-list">Home</NavLink></li>
                 <li><NavLink className="nav-list">About</NavLink></li>
                 <li><NavLink className="nav-list">Projects</NavLink></li>
                 <li><NavLink className="nav-list">Service</NavLink></li>
                 <li><NavLink className="nav-list">Contact</NavLink></li>
             </div>
-            {/* <button className="bg-white opacity-75 px-8 py-3 rounded-lg font-medium flex items-center text-lg">Hello <FaPaperPlane className="ml-2"></FaPaperPlane></button> */}
+
+            <div className="flex justify-between mx-2">
+                <div className="text-white text-5xl md:hidden">
+                    {
+                        !showMenu ?
+                            <HiMenuAlt2 onClick={() => setShowMenu(true)}></HiMenuAlt2>
+                            :
+                            <RxCross2 onClick={() => setShowMenu(false)}></RxCross2>
+                    }
+                </div>
+                <button className="bg-white opacity-75 px-8 py-3 rounded-lg font-medium flex items-center text-lg">Hello <FaPaperPlane className="ml-2"></FaPaperPlane></button>
+            </div>
         </div>
     );
 };
