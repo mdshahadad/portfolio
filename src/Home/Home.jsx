7d0ab22ab3a4b6f6ef1548d9abd231c439534753
mdshahadad <social.shahadad@gmail.com>
2024-02-14 16:26:57 +0600

    AOS Package fade and fade right use at the banner section with img and text

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,14 +3,25 @@ import './Home.css'
 
 const Home = () => {
     return (
-        <div className="bg-[#010101]">
+        <div className="bg-black w-full">
             <Navbar></Navbar>
-            <div className="relative">
-                <div className="flex justify-center mt-8">
+            <div
+                className="relative">
+                <div
+                    data-aos="fade-up"
+                    data-aos-duration="1400"
+                    data-aos-delay="50"
+                    className="flex justify-center mt-8"
+                >
                     <img className="z-20 hover:z-10" src="https://i.postimg.cc/FHk3HVM1/IMG-2036.png" alt="" />
                 </div>
-                <div className="flex justify-center fill-slate-50 ">
-                    <h1 className="text-white font-extrabold text-[170px] absolute top-32 text-stroke z-10 hover:z-30">WEB DEVELOPER</h1>
+
+                <div className="flex justify-center fill-slate-50 py-0">
+                    <h1
+                        data-aos="fade-right"
+                        data-aos-duration="3000" data-aos-easing="linear"
+                        data-aos-delay="1000"
+                        className="font-extrabold text-[170px] absolute top-32 text-stroke z-10 hover:z-30">WEB DEVELOPER</h1>
                 </div>
             </div>
         </div>
--- src/Home/Navbar/Navbar.jsx
+++ src/Home/Navbar/Navbar.jsx
@@ -4,15 +4,15 @@ import '../Home.css'
 const Navbar = () => {
     return (
         <div className="flex justify-between max-w-7xl mx-auto items-center pt-4">
-            <h2 className="text-white font-bold text-4xl">shahadad.</h2>
-            <div className="text-white text-center">
+            <h2 className="text-white opacity-60 font-bold text-4xl">shahadad.</h2>
+            <div className="text-white opacity-60 text-center">
                 <NavLink className="nav-list">Home</NavLink>
                 <NavLink className="nav-list">About</NavLink>
                 <NavLink className="nav-list">Projects</NavLink>
                 <NavLink className="nav-list">Service</NavLink>
                 <NavLink className="nav-list">Contact</NavLink>
             </div>
-            <button className="bg-white px-8 py-2 rounded-lg font-medium">Hello</button>
+            <button className="bg-white opacity-60 px-8 py-2 rounded-lg font-medium">Hello</button>
         </div>
     );
 };
