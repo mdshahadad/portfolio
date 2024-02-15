20c9878e6e64543841d1e4329712957426aa7fb9
mdshahadad <social.shahadad@gmail.com>
2024-02-15 12:59:38 +0600

    Banner responsive issues solve for max-width 320px and max-width 376px

--- src/Home/Banner/BannerText.jsx
+++ src/Home/Banner/BannerText.jsx
@@ -7,7 +7,7 @@ const BannerText = () => {
                 data-aos="fade-down"
                 data-aos-duration="3000" data-aos-easing="linear"
                 data-aos-delay="500"
-                className="font-extrabold text-[36px] md:text-[110px] lg:text-[170px] absolute top-32 md:40 lg:top-36 lg:left-[240px] text-stroke z-10 hover:z-30"
+                className="font-extrabold md:text-[110px] lg:text-[170px] md:40 lg:top-36 lg:left-[240px] text-stroke z-10 hover:z-30 bannerText"
             > <span className=''>WEB </span>
                 <span className=''>DEVELOPER</span>
             </h1>
