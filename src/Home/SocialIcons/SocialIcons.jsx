113c449fe8c9363d609cb76a139f27bc02836511
mdshahadad <social.shahadad@gmail.com>
2024-02-15 14:16:24 +0600

    Lets connect reposition for responsive

--- src/Home/SocialIcons/SocialIcons.jsx
+++ src/Home/SocialIcons/SocialIcons.jsx
@@ -17,7 +17,7 @@ const SocialIcons = () => {
                 <IoLogoGithub className="text-4xl mt-4 hover:text-white"></IoLogoGithub>
             </Link>
             
-            <h4 className="rotate-90 mt-14 text-xl -translate-x-[37px] hover:text-white">Let's connect</h4>
+            <h4 className="rotate-90 mt-14 mb-14 md:mb-0 text-xl -translate-x-[37px] hover:text-white ">Let's connect</h4>
             <div className="w-[2px] rounded-full mt-14 md:block h-28 md:bg-[#bfbfbf] hidden translate-x-[18px]"></div>
         </div>
     );
