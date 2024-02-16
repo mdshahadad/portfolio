480bfb995cf92766233e2fa6f22d41af66002c4e
mdshahadad <social.shahadad@gmail.com>
2024-02-17 00:28:26 +0600

    Added Modal by flowbite react

--- src/Home/Services/Services.jsx
+++ src/Home/Services/Services.jsx
@@ -1,14 +1,19 @@
+import { useState } from "react";
+import ServiceModal from "./ServiceModal";
 import ServicesCard from "./ServicesCard";
 import ServicesTitle from "./ServicesTitle";
 
 const Services = () => {
+    const [openModal, setOpenModal] = useState(false);
+
     return (
         <div className="bg-black mb-20">
             <div className="relative">
                 <ServicesTitle></ServicesTitle>
             </div>
             <h3 className="text-white opacity-65 font-medium text-3xl text-center mt-72 uppercase hover:opacity-100">Choose services you want</h3>
-            <ServicesCard></ServicesCard>
+            <ServicesCard setOpenModal={setOpenModal} openModal={openModal}></ServicesCard>
+            <ServiceModal setOpenModal={setOpenModal} openModal={openModal}></ServiceModal>
         </div>
     );
 };
--- src/Home/Services/ServicesCard.jsx
+++ src/Home/Services/ServicesCard.jsx
@@ -1,14 +1,14 @@
 import { IoIosArrowDroprightCircle } from "react-icons/io";
 import { TbDeviceImacCode } from "react-icons/tb";
-import { FaHeadphones } from "react-icons/fa";
+// import { FaHeadphones } from "react-icons/fa";
 import { BsFileEarmarkCodeFill } from "react-icons/bs";
 import { TbSettingsCode } from "react-icons/tb";
 import { HiCodeBracketSquare } from "react-icons/hi2";
 
 
-const ServicesCard = () => {
+const ServicesCard = ({ openModal, setOpenModal }) => {
     return (
-        <div className="max-w-7xl mx-auto py-10">
+        <div className="max-w-5xl mx-auto py-10">
             <div className="text-white grid grid-cols-2 gap-10 mt-12">
                 <div className="flex justify-center">
                     <div
@@ -16,20 +16,21 @@ const ServicesCard = () => {
                         data-aos-duration="1000"
                         data-aos-easing="ease-in"
                         className="">
-                        <div className="mb-10 bg-white hover:opacity-100 opacity-75 text-black px-16 py-12 rounded-xl w-full hover:scale-110 hover:transition-all">
+                        <div className="mb-10 bg-white hover:opacity-100 opacity-75 text-black px-16 py-12 rounded-xl w-[440px] hover:scale-110 hover:transition-all">
                             <h3
                                 className="text-2xl font-medium">
                                 <TbDeviceImacCode
                                     className="mr-2 text-7xl"
                                 /> Basic Website Development
                             </h3>
-                            <div className="mt-16 text-white flex justify-between gap-8">
-                                <h4 className="flex items-center text-xl cursor-pointer px-4 py-2 rounded-full bg-black ">View Details <IoIosArrowDroprightCircle className="text-3xl ml-2" /></h4>
+                            <div className="mt-16 text-white flex justify-start gap-8">
+                                <h4 onClick={() => setOpenModal(true)}
+                                    className="flex items-center text-xl cursor-pointer px-4 py-2 rounded-full bg-black">View Details <IoIosArrowDroprightCircle className="text-3xl ml-2" /></h4>
 
-                                <h4 className="flex items-center text-xl cursor-pointer px-7 py-2 rounded-full  bg-black ">Contact <FaHeadphones className="text-3xl ml-2" /></h4>
+                                {/* <h4 className="flex items-center text-xl cursor-pointer px-7 py-2 rounded-full  bg-black ">Contact <FaHeadphones className="text-3xl ml-2" /></h4> */}
                             </div>
                         </div>
-                        <div className="mb-10 bg-white hover:opacity-100 opacity-75 text-black px-16 py-12 rounded-xl w-full hover:scale-110 hover:transition-all">
+                        <div className="mb-10 bg-white hover:opacity-100 opacity-75 text-black px-16 py-12 rounded-xl w-[440px] hover:scale-110 hover:transition-all">
                             <h3
                                 className="text-2xl font-medium">
                                 <BsFileEarmarkCodeFill
@@ -39,7 +40,7 @@ const ServicesCard = () => {
                             <div className="mt-16 text-white flex justify-between gap-8">
                                 <h4 className="flex items-center text-xl cursor-pointer px-4 py-2 rounded-full bg-black ">View Details <IoIosArrowDroprightCircle className="text-3xl ml-2" /></h4>
 
-                                <h4 className="flex items-center text-xl cursor-pointer px-7 py-2 rounded-full  bg-black ">Contact <FaHeadphones className="text-3xl ml-2" /></h4>
+                                {/* <h4 className="flex items-center text-xl cursor-pointer px-7 py-2 rounded-full  bg-black ">Contact <FaHeadphones className="text-3xl ml-2" /></h4> */}
                             </div>
                         </div>
                     </div>
@@ -52,20 +53,20 @@ const ServicesCard = () => {
                         data-aos-duration="1000"
                         data-aos-easing="ease-in"
                         className="">
-                        <div className="mb-10 bg-white opacity-75 text-black px-16 py-12 rounded-xl w-full  hover:opacity-100 hover:scale-110 hover:transition-all">
+                        <div className="mb-10 bg-white opacity-75 text-black px-16 py-12 rounded-xl w-[440px] hover:opacity-100 hover:scale-110 hover:transition-all">
                             <h3
                                 className="text-2xl font-medium">
                                 <TbSettingsCode
                                     className="mr-2 text-7xl"
-                                /> Bug Fixing
+                                /> Bug Fixing for Website
                             </h3>
                             <div className="mt-16 text-white flex justify-between gap-8">
                                 <h4 className="flex items-center text-xl cursor-pointer px-4 py-2 rounded-full bg-black ">View Details <IoIosArrowDroprightCircle className="text-3xl ml-2" /></h4>
 
-                                <h4 className="flex items-center text-xl cursor-pointer px-7 py-2 rounded-full bg-black ">Contact <FaHeadphones className="text-3xl ml-2" /></h4>
+                                {/* <h4 className="flex items-center text-xl cursor-pointer px-7 py-2 rounded-full bg-black ">Contact <FaHeadphones className="text-3xl ml-2" /></h4> */}
                             </div>
                         </div>
-                        <div className="mb-10 bg-white hover:opacity-100 opacity-75 text-black px-16 py-12 rounded-xl w-full hover:scale-110 hover:transition-all">
+                        <div className="mb-10 bg-white hover:opacity-100 opacity-75 text-black px-16 py-12 rounded-xl w-[440px] hover:scale-110 hover:transition-all">
                             <h3
                                 className="text-2xl font-medium">
                                 <HiCodeBracketSquare
@@ -75,7 +76,7 @@ const ServicesCard = () => {
                             <div className="mt-16 text-white flex justify-between gap-8">
                                 <h4 className="flex items-center text-xl cursor-pointer px-4 py-2 rounded-full bg-black ">View Details <IoIosArrowDroprightCircle className="text-3xl ml-2" /></h4>
 
-                                <h4 className="flex items-center text-xl cursor-pointer px-7 py-2 rounded-full bg-black ">Contact <FaHeadphones className="text-3xl ml-2" /></h4>
+                                {/* <h4 className="flex items-center text-xl cursor-pointer px-7 py-2 rounded-full bg-black ">Contact <FaHeadphones className="text-3xl ml-2" /></h4> */}
                             </div>
                         </div>
                     </div>
