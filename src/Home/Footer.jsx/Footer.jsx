6051338afe9d1a9855d69a71ecb0f92e69c2ee3f
mdshahadad <social.shahadad@gmail.com>
2024-02-25 17:22:25 +0600

    Footer responsive for phone

--- src/Home/Footer.jsx/Footer.jsx
+++ src/Home/Footer.jsx/Footer.jsx
@@ -13,59 +13,64 @@ const Footer = () => {
     return (
         <div className="w-full bg-white opacity-75 text-center py-20 mt-12">
             <div className="md:max-w-4xl md:mx-auto">
-                <div className="mb-16">
+                <div className="mb-8 md:mb-16">
                     <h2 className="opacity-75 font-bold text-4xl md:text-7xl text-center mb-2 mt-1 md:mt-0 hover:opacity-100">shahadad.</h2>
-                    <p className="font-medium text-xl up">A Junior Web Developer In Town</p>
+                    <p className="font-medium text-xs md:text-xl">A Junior Web Developer In Town</p>
                 </div>
-                <div className="md:flex md:justify-between mt-5">
+                <div className="md:flex md:justify-between md:mt-5">
                     <div>
                         <ul className="flex justify-center md:justify-between font-medium text-lg">
-                            <li className="mr-4 text-2xl">
+                            <li className="mx-2 md:mr-4 text-xl md:text-2xl">
                                 <div className="flex justify-center">
-                                    <GoFileDirectoryFill className="text-5xl mb-2" />
+                                    <GoFileDirectoryFill className="text-5xl mb-2 hidden md:block" />
                                 </div>
-                                <NavLink to='/projects'>Projects</NavLink></li>
-                            <li className="mr-4 text-2xl">
+                                <NavLink to='/projects'>Projects</NavLink>
+                            </li>
+                            <li className="mx-2 md:mr-4 text-xl md:text-2xl">
                                 <div className="flex justify-center">
-                                    <TbDeviceImacCode className="text-5xl mb-2" />
+                                    <TbDeviceImacCode className="text-5xl mb-2 hidden md:block" />
                                 </div>
-                                <NavLink to='/services'>Services</NavLink></li>
-                            <li className="mr-4  text-2xl">
+                                <NavLink to='/services'>Services</NavLink>
+                            </li>
+                            <li className="mx-2 md:mr-4 text-xl  md:text-2xl">
                                 <div className="flex justify-center">
-                                    <FaHeadphones className="text-5xl mb-2" />
+                                    <FaHeadphones className="text-5xl mb-2 hidden md:block" />
                                 </div>
-                                <NavLink to='/contact'>Contact</NavLink></li>
+                                <NavLink to='/contact'>Contact</NavLink>
+                            </li>
                         </ul>
                     </div>
                     <div>
-                        <div className="flex justify-center md:justify-normal mt-12 md:mt-0">
+                        <div className="flex justify-center md:justify-normal mt-8 md:mt-0">
                             <div>
                                 <Link to={`https://www.linkedin.com/in/shahadad/`}>
-                                    <IoLogoLinkedin className="text-5xl md:mr-3 mr-5  cursor-pointer hover:text-[#0A6BCA]" />
+                                    <IoLogoLinkedin className="text-4xl md:text-5xl md:mr-3 mx-2 cursor-pointer hover:text-[#0A6BCA]" />
                                 </Link>
                             </div>
                             <div>
                                 <Link to={`https://github.com/mdshahadad`}>
-                                    <IoLogoGithub className="text-5xl md:mr-3 mr-5 cursor-pointer" />
+                                    <IoLogoGithub className="text-4xl md:text-5xl md:mr-3 mx-2 cursor-pointer" />
                                 </Link>
                             </div>
                             <div>
-                                <FaTelegram className="text-5xl md:mr-3 mr-5 cursor-pointer hover:text-[#2a9cd4]" />
+                                <FaTelegram className="text-4xl md:text-5xl md:mr-3 mx-2 cursor-pointer hover:text-[#2a9cd4]" />
                             </div>
                             <div>
-                                <FaWhatsapp className="text-5xl md:mr-3 mr-5 cursor-pointer hover:text-[#2bb741]" />
+                                <FaWhatsapp className="text-4xl md:text-5xl md:mr-3 mx-2 cursor-pointer hover:text-[#2bb741]" />
                             </div>
                             <div className="">
-                                <TbMailShare className="text-5xl md:mr-3 mr-5 cursor-pointer hover:text-[#ea3f41]" />
+                                <TbMailShare className="text-4xl md:text-5xl md:mr-3 mx-2 cursor-pointer hover:text-[#ea3f41]" />
                             </div>
                         </div>
 
-                        <h4 className="text-center font-medium text-xl mt-4 flex items-center justify-center">Say Hello To Me <FaPaperPlane className="ml-2" /></h4>
+                        <h4 className="text-center font-medium text-lg md:text-xl mt-4 flex items-center justify-center">Say Hello To Me <FaPaperPlane className="ml-2" /></h4>
                     </div>
                 </div>
-                <div className="mt-12 font-medium text-lg md:text-xl flex justify-center">
-                    <p className="flex items-center"> <LuCopyright className="mt-1 mr-2" />
-                        Copyright Mohammad Shahadad Hossain</p>
+                <div className="mt-12 font-medium text-base md:text-xl flex justify-center">
+                    <p className="flex items-center">
+                        <LuCopyright className="mr-2" />
+                        Copyright Mohammad Shahadad Hossain
+                    </p>
                 </div>
             </div>
         </div>
