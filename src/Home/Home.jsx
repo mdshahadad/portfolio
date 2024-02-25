db4f8aa91a5dfdd2e6ee8dac49efdf4c25ea8d04
mdshahadad <social.shahadad@gmail.com>
2024-02-25 15:05:25 +0600

    Every component call from home compo updated

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -19,22 +19,17 @@ const Home = () => {
             <BannerText></BannerText>
             <SocialIcons></SocialIcons>
             <MouseScroll></MouseScroll>
-            <div className="bg-black h-full">
-                <AboutMe></AboutMe>
-                <div className="bg-black mb-20">
-                    <div className="relative">
-                        <ServicesTitle></ServicesTitle>
-                    </div>
-                    <h3 className="text-white opacity-65 font-medium text-xl md:text-3xl text-center mt-28 md:mt-72 uppercase hover:opacity-100">Choose services you want</h3>
-                    <Services></Services>
-                </div>
-                <Skills></Skills>
-                <Projects></Projects>
-
-                <FAQ></FAQ>
-                <Contact></Contact>
-                <Footer></Footer>
+            <AboutMe></AboutMe>
+            <div className="relative">
+                <ServicesTitle></ServicesTitle>
             </div>
+            <h3 className="text-white opacity-65 font-medium text-xl md:text-3xl text-center mt-28 md:mt-72 uppercase hover:opacity-100">Choose services you want</h3>
+            <Services></Services>
+            <Skills></Skills>
+            <Projects></Projects>
+            <FAQ></FAQ>
+            <Contact></Contact>
+            <Footer></Footer>
         </div>
     );
 };
