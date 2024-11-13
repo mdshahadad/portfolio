30eb16e8aca9ce51c2f9106047c87d18c497ae02
mdshahadad <social.shahadad@gmail.com>
2024-11-14 01:49:35 +0600

    Added new project aschii.com

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -5,12 +5,27 @@ import { Link } from "react-router-dom";
 
 const Projects = () => {
     const [projects, setProjects] = useState([]);
+    const [showMore, setShowMore] = useState(false);
+    console.log(projects)
 
     useEffect(() => {
         fetch(`https://shahadad.vercel.app/projects`)
             .then(res => res.json())
-            .then(data => setProjects(data))
-    }, [])
+            .then(data => {
+                data.reverse();
+                console.log(data)
+                setProjects(data)
+            })
+    }, []);
+
+    const visibleProjects = showMore === true ? projects : projects.slice(0, 2);
+    // console.log(visibleProjects.length);
+
+    const handleShowMoreAndLess = () => {
+        setShowMore(!showMore);
+    }
+
+
     return (
         <div className="mb-20 relative">
 
@@ -24,18 +39,19 @@ const Projects = () => {
 
             <div className="-mt-44 md:-mt-0">
                 {
-                    projects.map(project => <ProjectsCard project={project} key={project._id}></ProjectsCard>)
+                    visibleProjects.map(project => <ProjectsCard project={project} key={project._id}></ProjectsCard>)
                 }
             </div>
 
             <div className="flex justify-center relative">
-                <Link to="/projects">
-                    <button
-                        className="text-black bg-white opacity-75 uppercase px-16 py-5 rounded-2xl font-medium hover:bg-transparent hover:text-white hover:scale-110  hover:outline-dotted flex items-center text-xl transition duration-300 ease-in-out">
-                        Show More
-                        <HiMiniArrowRightCircle className="text-4xl ml-2" />
-                    </button>
-                </Link>
+                <button
+                    onClick={handleShowMoreAndLess}
+                    className="text-black bg-white opacity-75 uppercase px-16 py-5 rounded-2xl font-medium hover:bg-transparent hover:text-white hover:scale-110  hover:outline-dotted flex items-center text-xl transition duration-300 ease-in-out">
+                    {
+                        showMore === true ? "Show Less" : "Show More"
+                    }
+                    <HiMiniArrowRightCircle className="text-4xl ml-2" />
+                </button>
             </div>
         </div>
     );
