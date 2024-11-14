f545be3dc497b3414b54e44c8430bd606720e05e
mdshahadad <social.shahadad@gmail.com>
2024-11-15 01:54:14 +0600

    everything is upto date

--- src/Contact/ContactIcons.jsx
+++ src/Contact/ContactIcons.jsx
@@ -1,10 +1,7 @@
 import { TbMailShare } from "react-icons/tb";
-import { IoLogoLinkedin } from "react-icons/io5";
-import { FaPaperPlane, FaWhatsapp } from "react-icons/fa";
+import { IoLogoGithub, IoLogoLinkedin } from "react-icons/io5";
+import { FaPaperPlane } from "react-icons/fa";
 import { FaTelegram } from "react-icons/fa";
-import { Link } from "react-router-dom";
-
-
 
 const ContactIcons = () => {
     return (
@@ -24,12 +21,10 @@ const ContactIcons = () => {
                             <h4 className="text-center font-semibold text-lg">LinkedIn</h4>
                             <p>in/shahadad/</p>
                             <div className="flex justify-center">
-                                <Link to='https://www.linkedin.com/in/shahadad/'>
-                                    <a
-                                        href="https://www.linkedin.com/in/shahadad/"
-                                        target="_blank"
-                                        className="flex items-center text-lg cursor-pointer px-8 py-2 rounded-full bg-black text-white mt-8 hover:bg-transparent border-2 border-black transition-all duration-700 hover:text-black" >Say Hello <FaPaperPlane className="ml-2 text-xl" /></a>
-                                </Link>
+                                <a
+                                    href="https://www.linkedin.com/in/shahadad/"
+                                    target="_blank"
+                                    className="flex items-center text-lg cursor-pointer px-8 py-2 rounded-full bg-black text-white mt-8 hover:bg-transparent border-2 border-black transition-all duration-700 hover:text-black" >Say Hello <FaPaperPlane className="ml-2 text-xl" /></a>
                             </div>
                         </div>
                     </div>
@@ -37,18 +32,16 @@ const ContactIcons = () => {
                 <div className="flex justify-center">
                     <div className="bg-white opacity-75 hover:opacity-100 hover:scale-110 duration-700 transition-all px-8 py-5 rounded-xl w-64 cursor-pointer icon-card mb-5 md:mb-0">
                         <div className="flex justify-center">
-                            <FaWhatsapp className="text-6xl mb-2 whatsapp" />
+                            <IoLogoGithub className="text-6xl mb-2 whatsapp" />
                         </div>
                         <div className="text-center">
-                            <h4 className="text-center font-semibold text-lg">Whatsapp</h4>
-                            <p>01690-103374</p>
+                            <h4 className="text-center font-semibold text-lg">Github</h4>
+                            <p>mdshahadad</p>
                             <div className="flex justify-center">
-                                <Link
-                                >
-                                    <a
-                                        href=""
-                                        className="flex items-center text-lg cursor-pointer px-8 py-2 rounded-full bg-black text-white mt-8 hover:bg-transparent border-2 border-black transition-all duration-700 hover:text-black" >Say Hello <FaPaperPlane className="ml-2 text-xl" /></a>
-                                </Link>
+                                <a
+                                    href="https://github.com/mdshahadad"
+                                    target="_blank"
+                                    className="flex items-center text-lg cursor-pointer px-8 py-2 rounded-full bg-black text-white mt-8 hover:bg-transparent border-2 border-black transition-all duration-700 hover:text-black" >Say Hello <FaPaperPlane className="ml-2 text-xl" /></a>
                             </div>
                         </div>
                     </div>
@@ -62,11 +55,9 @@ const ContactIcons = () => {
                             <h4 className="text-center font-semibold text-lg">Telegram</h4>
                             <p>mohammadshahadad</p>
                             <div className="flex justify-center">
-                                <Link to={`https://t.me/mohammadshahadad`}>
-                                    <a
-                                        href=""
-                                        className="flex items-center text-lg cursor-pointer px-8 py-2 rounded-full bg-black text-white mt-8  hover:bg-transparent border-2 border-black transition-all duration-700 hover:text-black" >Say Hello <FaPaperPlane className="ml-2 text-xl" /></a>
-                                </Link>
+                                <a
+                                    href=""
+                                    className="flex items-center text-lg cursor-pointer px-8 py-2 rounded-full bg-black text-white mt-8  hover:bg-transparent border-2 border-black transition-all duration-700 hover:text-black" >Say Hello <FaPaperPlane className="ml-2 text-xl" /></a>
                             </div>
                         </div>
                     </div>
